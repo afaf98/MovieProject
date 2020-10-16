8094beed05fa1cb2a7ed6c6a92500452c033e3e7
Afaf Ibrahimi <afaf.ibrahimi@gmail.com>
2020-10-16 11:01:24 +0200

    Adding Links and Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Switch, Route, NavLink, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <navBar>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/discover">Discover</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </navBar>
+      <Switch>
+        {/* <Route path="/discover" component={DiscoverMoviePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} /> */}
+      </Switch>
     </div>
   );
 }
